21c1b3f52690c81b9e3921bfc2c196fc32444530
bala-amypo <bala@amypo.in>
2025-12-15 16:55:52 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/LocationServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/LocationServiceImpl.java
@@ -2,16 +2,21 @@ package com.example.demo.service.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.Location;
+import com.example.demo.repository.LocationRepository;
 import com.example.demo.service.LocationService;
 @Service
 public class LocationServiceImpl implements LocationService{
    
-    @Autowired LocalRe
+    @Autowired LocationRepository location;
+
 
  public Location createLocation(Location location){
+    if(location)
+    return location.save(location);
     
 
  }
