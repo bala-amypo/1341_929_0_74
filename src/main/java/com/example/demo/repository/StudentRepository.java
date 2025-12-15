c134d4ee21b4da4d996fb8941bfa6cd8ab030b7c
bala-amypo <bala@amypo.in>
2025-12-15 18:10:32 +0000

    User commit

--- src/main/java/com/example/demo/repository/StudentRepository.java
+++ src/main/java/com/example/demo/repository/StudentRepository.java
@@ -3,7 +3,9 @@ package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.demo.entity.StudentEntity;
+
 @Repository
-public interface StudentRepository extends JpaRepository<Studnet {
+public interface StudentRepository extends JpaRepository<StudentEntity,Long> {
     
 }
--- src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
@@ -1,7 +1,25 @@
 package com.example.demo.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.StudentEntity;
+import com.example.demo.repository.StudentRepository;
+import com.example.demo.service.StudentService;
+
 @Service
 public class StudentServiceImpl implements StudentService{
+
+    @Autowired StudentRepository sturepo;
+    
+    @Override
+      public StudentEntity postdata(StudentEntity stu){
+        return sturepo.save(stu);
+
+      }
+      public List<
+
+
 
 
 }
